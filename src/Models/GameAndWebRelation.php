7d52d5f0306b363fb645911ef33f21b25dcb4fec
Jav <jav.valentin@gmail.com>
2021-06-21 08:57:10 +0200

    Update GameAndWebRelation.php

--- src/Models/GameAndWebRelation.php
+++ src/Models/GameAndWebRelation.php
@@ -9,6 +9,8 @@ use Illuminate\Database\Eloquent\Model;
 class GameAndWebRelation extends Model
 {
     public $timestamps = false;
+    
+    protected $table = 'dofus129_in_game_web_account_relations';
 
     protected $fillable = ['azuriom_id', 'dofus_id'];
 }
